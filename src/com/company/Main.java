c8f55aa02cd6ca44682bc0401ca129be201d63e0
Boldeusz <mnpower@poczta.onet.pl>
2020-11-08 14:31:59 +0100

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         boolean gameOver = true;
-        int score = 6000;
+        int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
 
@@ -17,5 +17,26 @@ public class Main {
         } else {
             System.out.println("Got here");
         }
+        if (gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
+        // Print out a second score on the screen with the following
+        // score set to 10000
+        // levelCompleted set to 8
+        // bonus set to 200
+        // But make sure the first printout above still displays as well
+
+        boolean newGameOver = true;
+        int newScore = 10000;
+        int newLevelCompleted = 8;
+        int newBonus = 200;
+
+        if (newGameOver) {
+            int finalScore = newScore + (newLevelCompleted * newBonus);
+            System.out.println("Your new final score was " + finalScore);
+        }
+
     }
 }
